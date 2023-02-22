09c6275b111ecf09fa85d9b945e2ca9fbef89f04
Lebogang Sekakeli <lebogangsekaleli@gmail.com>
2023-02-22 15:34:44 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import BarChart from './Charts/BarChart';
+import DoughnutChart from './Charts/DoughnutChart';
+import LineChart from './Charts/LineChart';
+import PieChart from './Charts/PieChart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BarChart />
+        {/* { <DoughnutChart />} */}
+        {/* { <LineChart /> } */}
+        {/* { <PieChart /> } */}
     </div>
   );
 }
